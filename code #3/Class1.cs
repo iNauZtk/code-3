0b8459be33079d8ea4738afac59a25e2f4acebb5
arria <arria@NAU>
2024-02-27 22:25:29 -0600

    Agregar archivos de proyecto.

---
+++ code #3/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class SymbolGenerator
+{
+    private const string Symbols = "!@#$%^&*()_+-=[]{}|;:'\",.<>/?";
+
+    // Constructor
+    public SymbolGenerator()
+    {
+        Console.WriteLine("Symbol Generator - Console App");
+    }
+
+    // Generate a random string with symbols
+    public string GenerateRandomString(int length)
+    {
+        Random random = new Random();
+        char[] symbolArray = Symbols.ToCharArray();
+        char[] resultArray = new char[length];
+
+        for (int i = 0; i < length; i++)
+        {
+            resultArray[i] = symbolArray[random.Next(symbolArray.Length)];
+        }
+
+        return new string(resultArray);
+    }
+}
---
+++ code #3/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // Create an instance of SymbolGenerator
+        SymbolGenerator symbolGenerator = new SymbolGenerator();
+
+        // Generate and display a random string with symbols
+        Console.Write("Enter the length of the random symbol string: ");
+        if (int.TryParse(Console.ReadLine(), out int length))
+        {
+            string randomSymbolString = symbolGenerator.GenerateRandomString(length);
+            Console.WriteLine($"Random Symbol String: {randomSymbolString}");
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid integer for the length.");
+        }
+    }
+}
